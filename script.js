98df3eaf7a7c7ce2abe1cf58c0064502413e917b
Raffee <Raffee1989@gmail.com>
2021-01-19 01:07:35 -0800

    Array of Questions added to JS

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-var questions = [{
-            ask = "If soccer is called football in England, what is American football called in England?",
-            options = ["American Football( )", "Combball ( )", "Handball( )", "Touchdown ( )" ]
-            
-
-    
-}]
+//Array of Questions, Options, Correct Answers!
+var questions = 
+    [
+        {
+            ask: "If soccer is called football in England, what is American football called in England?",
+            options: ["American Football( )", "Combball( )", "Handball( )", "Touchdown( )"],
+            response: ["American Footbal( )"]  
+        },
+        {   
+            ask: "What is the largest country in the world?",
+            options: ["Russia( )", "Canada( )", "China( )", "United States( )"],
+            response: "Russia( )"
+        },
+        {
+            ask: "A doctor with a Ph.D is a doctor of what?",
+            options: ["Philosophy( )", "Psychology( )", "Phrenology( )", "Physical Therapy( )"],
+            response: "Psychology( )"
+        },
+        {
+            ask: "What state is the largest state of the United States of America?",
+            options: ["Alaska( )", "California( )", "Texas( )", "Washington( )"],
+            response: "Alaska( )"
+        },
+        {
+            ask: "What is the fastest land animal?",
+            options: ["Cheetah( )", "Lion( )", "Thomson’s Gazelle( )", "Pronghorn Antelope( )"],
+            options: "Cheetah( )"
+        }   
+    ]
